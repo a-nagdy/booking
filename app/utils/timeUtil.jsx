608ef899f79ee45e1efb162b181ec1d4fef2c323
ahmed nagdy <ahmednagdy165@gmail.com>
2023-08-27 14:13:39 +0300

    fixing time should now be 1hour interval

--- app/utils/timeUtil.jsx
+++ app/utils/timeUtil.jsx
@@ -2,16 +2,16 @@ import dayjs from "dayjs";
 
 export const generateTimeOptions = (reservedTimes) => {
   const timeOptions = [];
-  const interval = 30; // Interval in minutes
-  const startTime = dayjs().startOf("day");
-  const endTime = dayjs().startOf("day").add(1, "day"); // Adjust end time as needed
+  const interval = 60; // Interval in minutes
+  const startTime = dayjs().hour(10).startOf("hour"); // Start at 10:00 AM
+  const endTime = dayjs().hour(12).startOf("hour"); // End at 12:00 PM (noon)
 
   for (
     let currentTime = startTime;
     currentTime.isBefore(endTime);
     currentTime = currentTime.add(interval, "minute")
   ) {
-    const formattedTime = currentTime.format("hh:mm A");
+    const formattedTime = currentTime.format("hh A"); // Format as hours only
 
     if (
       !reservedTimes.some((reservedTime) => reservedTime.time === formattedTime)
