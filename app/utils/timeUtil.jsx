922f5b3b3dd3bb78376375bf560371c796968c63
ahmed nagdy <ahmednagdy165@gmail.com>
2023-08-27 13:34:20 +0300

    fixing hour and min

--- app/utils/timeUtil.jsx
+++ app/utils/timeUtil.jsx
@@ -2,15 +2,10 @@ import dayjs from "dayjs";
 
 export const generateTimeOptions = (reservedTimes) => {
   const timeOptions = [];
-  const interval = 30; // Interval in minutes
   const startTime = dayjs().startOf("day");
   const endTime = dayjs().startOf("day").add(1, "day"); // Adjust end time as needed
 
-  for (
-    let currentTime = startTime;
-    currentTime.isBefore(endTime);
-    currentTime = currentTime.add(interval, "minute")
-  ) {
+  for (let currentTime = startTime; currentTime.isBefore(endTime); ) {
     const formattedTime = currentTime.format("hh:mm A");
 
     if (
